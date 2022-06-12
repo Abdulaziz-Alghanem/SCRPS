76b09fc49b8295c4bf4e49501fea283ed71661ef
Abdulaziz Alghanem <Ghanem-a@outlook.com>
2022-06-12 07:24:29 +0300

    added some functions

--- R/SCRPS.R
+++ R/SCRPS.R
@@ -8,9 +8,44 @@ SCRPS_norm <- function(y, mean = 0, sd = 1) {
 
 
 SCRPS_exp <- function(y, l = 1){
-   score <- -2*exp(-l*y) - l*y + 1 - 0.5*log(1/l)
-   return(score)
+  if (y <= 0) {
+    E1 <- 1/l - y
+  } else {
+    E1 <- (2*exp(-l*y))/l + y - 1/l
   }
+  E2 <- 1/l
+  score <- -E1/E2 - 0.5*log(E2)
+  return(score)
+}
+
+
+SCRPS_unif <- function(y, min = 0, max = 1){
+  if (y <= min) {
+    E1 <- 0.5*(min + max) - y
+  } else if (y >= max) {
+    E1 <- y - 0.5*(min + max)
+  } else {
+    E1 <- (y*(y-min-max)+0.5*(min^2 + max^2))/(max-min)
+  }
+
+  E2 <- (max - min)/(3)
+  score <- -E1/E2 -0.5*log(E2)
+  return(score)
+}
+
+
+
+
+SCRPS_exp2 <- function(y, l = 1, a = 0, b = 1){
+  if (y <= a) {
+    E1 <- a + b/l - y
+  } else {
+    E1 <- (b/l)*(2*exp(-l*((y-a)/b)) - 1) + y - a
+  }
+  E2 <- b/l
+  score <- -E1/E2 - 0.5*log(E2)
+  return(score)
+}
 
 
 
