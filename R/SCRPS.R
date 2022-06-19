8a10c1d26dd24e3a5cf796d5bb2c378ee5b8d204
Abdulaziz Alghanem <Ghanem-a@outlook.com>
2022-06-19 10:46:50 +0300

    SCRPS for a few more distributions

--- R/SCRPS.R
+++ R/SCRPS.R
@@ -2,9 +2,11 @@
 
 SCRPS_norm <- function(y, mean = 0, sd = 1) {
   z <- (mean-y) /sd
-  score <- -sqrt(pi)*dnorm(z) - sqrt(pi)*z/2 * (2*pnorm(z) - 1) - 0.5*log(2*s/sqrt(pi))
+  score <- -sqrt(pi)*dnorm(z) - sqrt(pi)*z/2 * (2*pnorm(z) - 1) - 0.5*log(2*sd/sqrt(pi))
+
   return(score)
 }
+
 
 
 SCRPS_exp <- function(y, l = 1){
@@ -17,7 +19,6 @@ SCRPS_exp <- function(y, l = 1){
   score <- -E1/E2 - 0.5*log(E2)
   return(score)
 }
-
 
 SCRPS_unif <- function(y, min = 0, max = 1){
   if (y <= min) {
@@ -35,7 +36,6 @@ SCRPS_unif <- function(y, min = 0, max = 1){
 
 
 
-
 SCRPS_exp2 <- function(y, l = 1, a = 0, b = 1){
   if (y <= a) {
     E1 <- a + b/l - y
@@ -46,6 +46,49 @@ SCRPS_exp2 <- function(y, l = 1, a = 0, b = 1){
   score <- -E1/E2 - 0.5*log(E2)
   return(score)
 }
+
+
+SCRPS_logis <- function(y, mu = 0, sd = 1){
+  y <- (y-mu)/sd
+  E1 <- 2*log(1 + exp(y)) -y
+
+  E2 <- 2
+  score <- -E1/E2 - 0.5*log(E2)
+  score <- score - 0.5 * log(sd)
+  return(score)
+}
+
+
+SCRPS_lapl <- function(y, mu = 0, b = 1){
+  y <- (y-mu)/b
+  if (y <= 0) {
+    E1 <- exp(y) -y
+  } else {
+    E1 <- exp(-y) +y
+  }
+  E2 <- 3/2
+  score <- -E1/E2 - 0.5*log(E2)
+  score <- score - 0.5*log(b)
+  return(score)
+}
+
+
+
+SCRPS_2pnorm <- function(y, mu = 0, s1 = 1, s2 = 1){
+  erf <- function(x) 2 * pnorm(x * sqrt(2)) - 1
+  erfc <- function(x) 2 * pnorm(x * sqrt(2), lower = FALSE)
+  y <- (y-mu)
+  if (y <= 0) {
+    E1 <- ( (-1 + 2*exp((-y^2)/(2*s1^2)))*sqrt(2/pi)*s1^2 + sqrt(2/pi)*s2^2 + y*s1 - y*s2 + 2*s1*y*erf(y/(sqrt(2)*s1))  ) /(s1 + s2)
+  } else {
+    E1 <- (sqrt(2/pi)*s1^2 - sqrt(2/pi)*s2^2 + 2*exp(-y^2/(2*s2^2))*sqrt(2/pi)*s2^2 + s1*y + s2*y*erf(y/(sqrt(2)*s2)) -s2*y*erfc(y/sqrt(2)*s2)) / (s1 + s2)
+  }
+  E2 <-  (-2*(-2 + sqrt(2))*s1^2 + 4*(-1 + sqrt(2))*s1*s2 -2*(-2 + sqrt(2))*s2^2) / (sqrt(pi)*(s1 + s2))
+  score <- -E1/E2 - 0.5*log(E2)
+  return(score)
+}
+
+
 
 
 
---
+++ tests/testthat/test.SCRPS_functions.R
@@ -0,0 +1,270 @@
+
+
+test_that("SCRPS for normal distribution", {
+
+  n <- 100000
+  k <- 50
+  count <- 0
+  for (i in 1:k) {
+    y1 <- runif(1, min = -1000, max = 1000)
+    m <- runif(1, min = -100, max = 100)
+    s  <- runif(1, min = 1, max = 50)
+
+    score1 <- SCRPS_norm(y1,mean = m, sd = s)
+    sample1 <- rnorm(n,mean = m, sd = s)
+    E1 <- mean(abs(sample1 - y1))
+    sample1 <- rnorm(n,mean = m, sd = s)
+    sample2 <- rnorm(n,mean = m, sd = s)
+    E2 <- mean(abs(sample1 - sample2))
+    score2 <- -E1/E2 - 0.5*log(E2)
+
+    if (abs((score1 - score2)/(score1)) < 0.05) {
+      count <- count + 1
+    }
+
+  }
+
+  accuracy = count/k
+
+  expect_gt(accuracy, 0.95)
+
+
+
+
+
+
+
+
+
+
+
+  } )
+
+
+
+
+test_that("SCRPS for exponential distribution", {
+
+  n <- 100000
+  k <- 50
+  count <- 0
+  for (i in 1:k) {
+    y1 <- runif(1, min = -1000, max = 1000)
+    l1 <- runif(1, min = 1, max = 10)
+
+    score1 <- SCRPS_exp(y1,l=l1)
+    sample1 <- rexp(n,rate=l1)
+    E1 <- mean(abs(sample1 - y1))
+    sample1 <- rexp(n,rate=l1)
+    sample2 <- rexp(n,rate=l1)
+    E2 <- mean(abs(sample1 - sample2))
+    score2 <- -E1/E2 - 0.5*log(E2)
+
+    if (abs((score1 - score2)/(score1)) < 0.05) {
+      count <- count + 1
+    }
+
+  }
+
+
+
+  accuracy = count/k
+
+  expect_gt(accuracy, 0.95)
+
+
+} )
+
+
+
+
+
+test_that("SCRPS for uniform distribution", {
+
+  n <- 100000
+  k <- 50
+  count <- 0
+  for (i in 1:k) {
+    y1 <- runif(1, min = -1000, max = 1000)
+    a <- runif(1, min = -100, max = 100)
+    b <- runif(1, min = a + 1, max = 150)
+    score1 <- SCRPS_unif(y1,min = a, max = b)
+    sample1 <- runif(n,min = a, max = b)
+    E1 <- mean(abs(sample1 - y1))
+    sample1 <- runif(n,min = a, max = b)
+    sample2 <- runif(n,min = a, max = b)
+    E2 <- mean(abs(sample1 - sample2))
+    score2 <- -E1/E2 - 0.5*log(E2)
+
+    if (abs((score1 - score2)/(score1)) < 0.05) {
+      count <- count + 1
+    }
+
+  }
+
+  accuracy = count/k
+
+  expect_gt(accuracy, 0.95)
+
+
+} )
+
+
+
+test_that("SCRPS for exponential distribution with flexible location and scale", {
+
+  n <- 100000
+  k <- 50
+  count <- 0
+  for (i in 1:k) {
+    y1 <- runif(1, min = -1000, max = 1000)
+    l1 <- runif(1, min = 1, max = 10)
+    a1 <- runif(1, min = -100, max = 100)
+    b1 <- runif(1, min = 1, max = 50)
+
+    score1 <- SCRPS_exp2(y1,l=l1, a = a1, b = b1)
+    sample1 <- rexp(n,rate=l1)
+    sample1 <- a1 + b1*sample1
+    E1 <- mean(abs(sample1 - y1))
+    sample1 <- rexp(n,rate=l1)
+    sample1 <- a1 + b1*sample1
+    sample2 <- rexp(n,rate=l1)
+    sample2 <- a1 + b1*sample2
+    E2 <- mean(abs(sample1 - sample2))
+    score2 <- -E1/E2 - 0.5*log(E2)
+
+
+    if (abs((score1 - score2)/(score1)) < 0.05) {
+      count <- count + 1
+    }
+
+  }
+
+
+
+
+  accuracy = count/k
+
+  expect_gt(accuracy, 0.95)
+
+
+} )
+
+
+
+
+test_that("SCRPS for logistic distribution", {
+
+  n <- 100000
+  k <- 50
+  count <- 0
+  for (i in 1:k) {
+    y1 <- runif(1, min = -1000, max = 1000)
+    m <- runif(1, min = -100, max = 100)
+    s  <- runif(1, min = 1, max = 50)
+
+    score1 <- SCRPS_logis(y1,mu = m, sd = s)
+    sample1 <- rlogis(n,location = m, scale = s)
+    E1 <- mean(abs(sample1 - y1))
+    sample1 <- rlogis(n,location = m, scale = s)
+    sample2 <- rlogis(n,location = m, scale = s)
+    E2 <- mean(abs(sample1 - sample2))
+    score2 <- -E1/E2 - 0.5*log(E2)
+
+    if (abs((score1 - score2)/(score1)) < 0.05) {
+      count <- count + 1
+    }
+
+  }
+
+
+  accuracy = count/k
+
+  expect_gt(accuracy, 0.95)
+
+
+} )
+
+
+
+test_that("SCRPS for laplace distribution", {
+
+  library("ExtDist")
+  n <- 100000
+  k <- 50
+  count <- 0
+  for (i in 1:k) {
+    y1 <- runif(1, min = -1000, max = 1000)
+    m <- runif(1, min = -100, max = 100)
+    s  <- runif(1, min = 1, max = 50)
+
+    score1 <- SCRPS_lapl(y1,mu = m, b = s)
+    sample1 <- rLaplace(n,mu = m, b = s)
+    E1 <- mean(abs(sample1 - y1))
+    sample1 <- rLaplace(n,mu = m, b = s)
+    sample2 <- rLaplace(n,mu = m, b = s)
+    E2 <- mean(abs(sample1 - sample2))
+    score2 <- -E1/E2 - 0.5*log(E2)
+
+    if (abs((score1 - score2)/(score1)) < 0.05) {
+      count <- count + 1
+    }
+
+  }
+
+
+
+
+
+  accuracy = count/k
+
+  expect_gt(accuracy, 0.95)
+
+
+} )
+
+
+
+test_that("SCRPS for 2 piece normal distribution", {
+
+  library("fanplot")
+
+  n <- 100000
+  k <- 50
+  count <- 0
+  for (i in 1:k) {
+    y1 <- runif(1, min = -1000, max = 1000)
+    m <- runif(1, min = -100, max = 100)
+    s1  <- runif(1, min = 1, max = 50)
+    s2  <- runif(1, min = 1, max = 50)
+
+    score1 <- SCRPS_2pnorm(y1,mu = m, s1 = s1, s2 = s2)
+    sample1 <- rsplitnorm(n, mode = m, sd1 = s1, sd2 = s2)
+    E1 <- mean(abs(sample1 - y1))
+    sample1 <- rsplitnorm(n, mode = m, sd1 = s1, sd2 = s2)
+    sample2 <- rsplitnorm(n, mode = m, sd1 = s1, sd2 = s2)
+    E2 <- mean(abs(sample1 - sample2))
+    score2 <- -E1/E2 - 0.5*log(E2)
+
+    if (abs((score1 - score2)/(score1)) < 0.05) {
+      count <- count + 1
+    }
+
+  }
+
+
+
+
+
+  accuracy = count/k
+
+  expect_gt(accuracy, 0.95)
+
+
+} )
+
+
+
+
+
+
