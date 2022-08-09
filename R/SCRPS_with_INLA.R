edbc7a3ce6842d70d14366b8aa24606fe71c9733
Abdulaziz Alghanem <Ghanem-a@outlook.com>
2022-08-09 13:31:49 +0300

    Some updates to the code structure

--- R/SCRPS_with_INLA.R
+++ R/SCRPS_with_INLA.R
@@ -1,5 +1,6 @@
 #' Title
 #'
+#'
 #' @param x b
 #' @param ... b
 #'
@@ -42,7 +43,7 @@ scrps <- function(x, ...) {
 #'
 #' @examples
 #' #to be completed
-scrps.inla <- function(inla_result, n = 10000,
+scrps.inla <- function(inla_result,scoring_rule = "scrps", n = 10000,
                             test_data = NULL,
                             full_y = NULL,
                             test_idx = NULL,
@@ -101,7 +102,7 @@ scrps.inla <- function(inla_result, n = 10000,
     # = foreach::`%dopar%`(foreach::foreach(i = 1:nrow(inla_result$summary.fitted.values)), {
     score = foreach::`%dopar%`(foreach::foreach(i = test_idx_complete), {
 
-      temp <- inla_score_for_i(inla_result = inla_result, y = y_complete[i], s = s[i], i = i, n = n)
+      temp <- inla_score_for_i(inla_result = inla_result, y = y_complete[i], s = s[i], i = i, n = n, scoring_rule = scoring_rule)
       score = mean(temp)
       return(score)
     })
@@ -115,7 +116,7 @@ scrps.inla <- function(inla_result, n = 10000,
     #score = lapply(which(!is.na(y_complete)), function(i){
     score = lapply(test_idx_complete, function(i){
 
-      temp <- inla_score_for_i(inla_result = inla_result, y = y_complete[i], s = s[i], i = i, n = n)
+      temp <- inla_score_for_i(inla_result = inla_result, y = y_complete[i], s = s[i], i = i, n = n, scoring_rule = scoring_rule)
       score = mean(temp)
       return(score)
     })
@@ -140,7 +141,7 @@ scrps.inla <- function(inla_result, n = 10000,
 
 
 
-inla_score_for_i <- function(inla_result, y, s, i, n) {
+inla_score_for_i <- function(inla_result, y, s, i, n, scoring_rule) {
 
   #################################################################
   ####################      "Gaussian"      #########################
@@ -151,7 +152,15 @@ inla_score_for_i <- function(inla_result, y, s, i, n) {
 
     sd_sample <- 1/sqrt(tau_sample * s)
 
-    temp <- SCRPS::SCRPS_norm(y, mean = mu_sample, sd = sd_sample)
+
+    if (scoring_rule == "scrps") {
+      temp <- SCRPS::SCRPS_norm(y, mean = mu_sample, sd = sd_sample)
+
+    } else if (scoring_rule == "crps") {
+
+      temp <- -scoringRules::crps_norm(y, mean = mu_sample, sd = sd_sample)
+
+    }
     return(temp)
 
 
@@ -167,7 +176,46 @@ inla_score_for_i <- function(inla_result, y, s, i, n) {
 
     shape_sample <- phi_sample * s
     scale_sample <- mu_sample / shape_sample
-    temp <- SCRPS::SCRPS_gamma(y, shape = shape_sample, scale = scale_sample)
+
+
+    if (scoring_rule == "scrps") {
+      temp <- SCRPS::SCRPS_gamma(y, shape = shape_sample, scale = scale_sample)
+
+    } else if (scoring_rule == "crps") {
+
+      temp <- -scoringRules::crps_gamma(y, shape = shape_sample, scale = scale_sample)
+
+    }
+
+
+
+    return(temp)
+
+
+
+
+
+    #################################################################
+    ####################      "Log Normal"      #########################
+    #################################################################
+  } else if (inla_result$.args$family == "lognormal") {
+
+
+    mu_sample <- INLA::inla.rmarginal(n, inla_result$marginals.fitted.values[[i]])
+    tau_sample <- INLA::inla.rmarginal(n,inla_result$marginals.hyperpar$`Precision for the lognormal observations`)
+
+    sd_sample <- 1/sqrt(tau_sample)
+
+    if (scoring_rule == "scrps") {
+      temp <- SCRPS::SCRPS_lnorm(y, meanlog = mu_sample, sdlog = sd_sample)
+
+    } else if (scoring_rule == "crps") {
+
+      temp <- -scoringRules::crps_lnorm(y, meanlog = mu_sample, sdlog = sd_sample)
+
+    }
+
+
     return(temp)
 
 
@@ -181,239 +229,6 @@ inla_score_for_i <- function(inla_result, y, s, i, n) {
 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-#######################
-#######################
-#######################
-#######################
-#######################
-#######################
-#######################
-#######################
-#######################
-#######################
-#######################
-#######################
-
-
-
-
-
-
-
-
-
-
-#' CRPS for INLA objects
-#'
-#' @param inla_result inla result object
-#' @param n bla
-#' @param test_data ble
-#' @param full_y bli
-#' @param test_idx blo
-#' @param parallelize blu
-#' @param n.cores bla
-#'
-#' @return CRPS
-#' @export
-#'
-#' @examples
-#'
-#' print("To be completed")
-
-CRPS_inla <- function(inla_result, n = 10000,
-                       test_data = NULL,
-                       full_y = NULL,
-                       test_idx = NULL,
-                       only_test_data = TRUE,
-                       parallelize = FALSE,
-                       n.cores = parallel::detectCores()-1) {
-
-  if (inla_result$.args$family == "gaussian") {
-
-    return(CRPS_inla_norm(inla_result = inla_result , n = n,
-                           test_data,
-                           full_y,
-                           test_idx,
-                           parallelize = parallelize,
-                           n.cores = n.cores))
-
-  } else if (inla_result$.args$family == "gamma") {
-
-
-    return(CRPS_inla_gamma(inla_result = inla_result , n = n,
-                            test_data,
-                            full_y,
-                            test_idx,
-                            parallelize = parallelize,
-                            n.cores = n.cores))
-
-  }
-
-
-
-
-
-
-
-}
-
-
-
-CRPS_inla_norm <- function(inla_result, n = 10000,
-                            test_data,
-                            full_y,
-                            test_idx,
-                            parallelize,
-                            n.cores) {
-
-
-
-  if(!is.null(test_data) || !is.null(full_y)){
-    if(!is.null(full_y)){
-      if(is.null(test_idx)){
-        stop("You should provide the indexes for the test data in test_idx!")
-      }
-      y_complete <- full_y
-    } else{
-      y_complete <- inla_result$.args$data$y
-      y_complete[is.na(y_complete)] <- test_data
-    }
-  }
-
-  if(parallelize){
-    cs <- parallel::makeCluster(n.cores)
-    doParallel::registerDoParallel(cs)
-    i <- 0
-    score = foreach::`%dopar%`(foreach::foreach(i=1:nrow(inla_result$summary.fitted.values)),  {
-
-      mu_sample <- INLA::inla.rmarginal(n, inla_result$marginals.fitted.values[[i]])
-      theta_sample <- INLA::inla.rmarginal(n,inla_result$marginals.hyperpar[[1]])
-      y <- y_complete[i]
-
-
-      temp <- scoringRules::crps_norm(y, mean = mu_sample, sd = 1/sqrt(theta_sample))
-      score = mean(temp)
-      return(score)
-    })
-
-
-    scores = unlist(score)
-    parallel::stopCluster(cs)
-    return(mean(scores))
-  } else{
-    score = lapply(1:nrow(inla_result$summary.fitted.values), function(i){
-
-      mu_sample <- INLA::inla.rmarginal(n, inla_result$marginals.fitted.values[[i]])
-      theta_sample <- INLA::inla.rmarginal(n,inla_result$marginals.hyperpar[[1]])
-      y <- y_complete[i]
-
-
-      temp <- scoringRules::crps_norm(y, mean = mu_sample, sd = 1/sqrt(theta_sample))
-      score = mean(temp)
-      return(score)
-    })
-
-
-    scores = unlist(score)
-    return(mean(scores))
-  }
-
-
-
-}
-
-
-
-CRPS_inla_gamma <- function(inla_result, n = 10000,
-                             test_data,
-                             full_y,
-                             test_idx,
-                             parallelize,
-                             n.cores) {
-
-  if(!is.null(test_data) || !is.null(full_y)){
-    if(!is.null(full_y)){
-      if(is.null(test_idx)){
-        stop("You should provide the indexes for the test data in test_idx!")
-      }
-      y_complete <- full_y
-    } else{
-      y_complete <- inla_result$.args$data$y
-      y_complete[is.na(y_complete)] <- test_data
-    }
-  } else{
-    y_complete <- inla_result$.args$data$y
-  }
-
-
-  s_full <- inla_result$.args$scale
-  if(length(s_full)==1){
-    s <- rep(s_full, length.out = nrow(inla_result$summary.fitted.values))
-  } else{
-    s <- s_full
-    rm(s_full)
-  }
-
-
-
-  if(parallelize){
-    cs <-  parallel::makeCluster(n.cores)
-    doParallel::registerDoParallel(cs)
-    i <- 0
-    score = foreach::`%dopar%`(foreach::foreach(i = 1:nrow(inla_result$summary.fitted.values)), {
-
-      mu_sample <- INLA::inla.rmarginal(n, inla_result$marginals.fitted.values[[i]])
-      phi_sample <- INLA::inla.rmarginal(n,inla_result$marginals.hyperpar$`Precision parameter for the Gamma observations`)
-
-      shape_sample <- phi_sample * s[i]
-      scale_sample <- mu_sample / shape_sample
-      y <- y_complete[i]
-
-      temp <- scoringRules::crps_gamma(y, shape = shape_sample, scale = scale_sample)
-      score = mean(temp)
-      return(score)
-    })
-
-
-    scores = unlist(score)
-    parallel::stopCluster(cs)
-    return(mean(scores))
-  } else{
-    #score = lapply(1:nrow(inla_result$summary.fitted.values), function(i){
-    score = lapply(which(!is.na(y_complete)), function(i){
-
-      mu_sample <- INLA::inla.rmarginal(n, inla_result$marginals.fitted.values[[i]])
-      phi_sample <- INLA::inla.rmarginal(n,inla_result$marginals.hyperpar$`Precision parameter for the Gamma observations`)
-
-      shape_sample <- phi_sample * s[i]
-      scale_sample <- mu_sample / shape_sample
-      y <- y_complete[i]
-
-      temp <- scoringRules::crps_gamma(y, shape = shape_sample, scale = scale_sample)
-      score = mean(temp)
-      return(score)
-    })
-
-
-    scores = unlist(score)
-    return(mean(scores))
-    #return(scores)
-  }
-
-
-
-}
 
 
 
